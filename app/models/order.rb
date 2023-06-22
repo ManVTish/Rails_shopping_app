a66f6f6667a4919559257b86d761e28a4fcb9ac3
Manish Roy <manishroy0112@gmail.com>
2023-06-22 18:43:51 +0530

    order validation added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :items, as: :itemable
   belongs_to :user
+
+  validates :status, presence: true
+  validates :comment, presence: true
 end
--- db/migrate/20230622114728_create_orders.rb
+++ db/migrate/20230622114728_create_orders.rb
@@ -2,8 +2,8 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.bigint :user_id
-      t.string :status
-      t.string :comment
+      t.string :status, default: "started"
+      t.string :comment, default: "order placed"
 
       t.timestamps
     end
