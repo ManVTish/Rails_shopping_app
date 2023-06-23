e785b02b67ff223792b9ddfbcd2a7937156d2da7
Manish Roy <manishroy0112@gmail.com>
2023-06-23 14:38:08 +0530

    spec added for cat & comm

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:content) }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:commentable) }
+  end
 end
