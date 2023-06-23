fe0626f82f06c5ac7d505a645c6fb73f52b88939
Manish Roy <manishroy0112@gmail.com>
2023-06-23 17:39:42 +0530

    spec update

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Review, type: :model do
   describe 'Associations' do
     it { is_expected.to belong_to(:product) }
     it { is_expected.to belong_to(:user) }
-    it { is_expected.to have_many(:comments) }
+    it { is_expected.to have_one(:comment) }
   end
 end
