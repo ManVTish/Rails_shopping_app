2acf93f640938468992ec801ae940b2c77f72160
Manish Roy <manishroy0112@gmail.com>
2023-06-26 16:50:25 +0530

    db seeded with fake-store api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+  # Seed Categories data
+  cat_res = JSON.parse(RestClient.get("https://fakestoreapi.com/products/categories"))
+  cat_res.each do |cat|
+    Category.create!(name: cat)
+  end
+
+  # Seed Products Data
+  pro_res = JSON.parse(RestClient.get('https://fakestoreapi.com/products'))
+  pro_res.each do |p|
+    Product.create!(
+      name: p['title'],
+      description: p['description'],
+      stock: p['rating']['count'],
+      price: p['price'],
+      category_id: case p['category']
+                   when 'electronics'
+                     1
+                   when 'jewelery'
+                     2
+                   when "men's clothing"
+                     3
+                   else
+                     4
+                   end
+    )
+  end
+end
