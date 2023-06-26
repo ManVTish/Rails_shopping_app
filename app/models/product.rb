0777f2dfa298abc085fcd527c878fb7cb1265918
Manish Roy <manishroy0112@gmail.com>
2023-06-27 01:13:36 +0530

    updated validation, added method for downloading image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ApplicationRecord
+  has_many_attached :images
   has_many :reviews
   belongs_to :category
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def download_image(image_url)
+    image_source = URI.parse(image_url)
+    filename = File.basename(image_source.path)
+
+    # TODO: this is a security vulnerability
+    file = URI.open(image_source)
+    images.attach(io: file, filename: filename)
+  end
 end
