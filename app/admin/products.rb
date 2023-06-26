db1794666070d7de8c859affb04c2f569dab85b7
Manish Roy <manishroy0112@gmail.com>
2023-06-27 01:14:16 +0530

    admin product created updated for file upload

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Product do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
-  permit_params :name, :description, :stock, :price, :category_id
+  permit_params :name, :description, :stock, :price, :category_id, images: []
 
   form do |f|
     f.inputs do
@@ -11,6 +11,7 @@ ActiveAdmin.register Product do
       f.input :description, require: false
       f.input :stock
       f.input :price
+      f.input :images, as: :file, input_html: { multiple: true }
       f.input :category_id, as: :select, collection: Category.all
       f.semantic_errors *f.object.errors.attribute_names
     end
@@ -23,19 +24,14 @@ ActiveAdmin.register Product do
     def create
       attrs = params[:product]
 
-      name = attrs[:name]
-      desc = attrs[:description] || ""
-      stock = attrs[:stock]
-      price = attrs[:price]
-      cat_id = attrs[:category_id]
-
-      @product = Product.new(
-        name: name,
-        description: desc,
-        stock: stock,
-        price: price,
-        category_id: cat_id
+      @product = Product.create!(
+        name: attrs[:name],
+        description: attrs[:description] || '',
+        stock: attrs[:stock],
+        price: attrs[:price],
+        category_id: attrs[:category_id]
       )
+      @product.images.attach(params[:product][:images])
 
       if @product.save
         redirect_to '/admin/products'
@@ -43,5 +39,16 @@ ActiveAdmin.register Product do
         render :new
       end
     end
+  end
+
+  show do
+    if resource.images.attached?
+      attributes_table(*resource.attributes.keys) do
+        row "Photos" do |resource|
+          resource.images.map { |photo| image_tag url_for photo }
+        end
+      end
+    end
+    # default_main_content
   end
 end
