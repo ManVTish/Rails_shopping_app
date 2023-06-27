a9e18d9a94e565c9addde97147c29c029e6617f0
Manish Roy <manishroy0112@gmail.com>
2023-06-28 01:50:12 +0530

    helper defined in seeds, removed from model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,13 +5,4 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
-  def download_image(image_url)
-    image_source = URI.parse(image_url)
-    filename = File.basename(image_source.path)
-
-    # TODO: this is a security vulnerability
-    file = URI.open(image_source)
-    images.attach(io: file, filename: filename)
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
 if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
@@ -12,6 +8,16 @@ if Rails.env.development?
   cat_res = JSON.parse(RestClient.get("https://fakestoreapi.com/products/categories"))
   cat_res.each do |cat|
     Category.create!(name: cat)
+  end
+
+  # image download helper
+  def download_image(image_url)
+    image_source = URI.parse(image_url)
+    filename = File.basename(image_source.path)
+
+    # TODO: this is a security vulnerability
+    file = URI.open(image_source)
+    [file, filename]
   end
 
   # Seed Products Data
@@ -33,6 +39,7 @@ if Rails.env.development?
                      4
                    end
     )
-    product.download_image(p['image'])
+    product_images = download_image(p['image'])
+    product.images.attach(io: product_images.first, filename: product_images.last)
   end
 end
