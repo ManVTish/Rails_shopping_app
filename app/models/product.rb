adb16c831c06b6982853c81ca28c3f904c61859a
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2023-07-03 14:56:35 +0530

    Fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def primary_image
+    images.first
+  end
 end
