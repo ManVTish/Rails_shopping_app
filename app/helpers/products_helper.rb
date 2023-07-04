15d4ba94443dd58e3b7612c3621fd3c71dda4d56
Manish Roy <manishroy0112@gmail.com>
2023-07-04 12:11:13 +0530

    method extracted to product helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,16 @@ module ProductsHelper
 
     query[:category_id_in].first.include?(category_id.to_s)
   end
+
+  def sort_by_selected?
+    options_for_select(
+      [
+        [t('Default'), ''],
+        [t('Price low to high'), 'price asc'],
+        [t('Price high to low'), 'price desc'],
+        [t('Latest products'), 'created_at desc']
+      ],
+      params.dig(:query, :s)
+    )
+  end
 end
