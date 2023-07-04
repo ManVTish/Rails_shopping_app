14260d9aeff9bfa9d79584864f3625df654daf74
Manish Roy <manishroy0112@gmail.com>
2023-07-05 01:19:07 +0530

    fixed categories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :categories, class: 'Category' do
+  factory :category, class: 'Category' do
+    name { Faker::Name.name }
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :products, class: 'Product' do
+  factory :product, class: 'Product' do
+    name { Faker::Name.name }
+    description { Faker::Lorem.sentence }
+    price { Faker::Number.number(digits: 1) }
+    stock { Faker::Number.number(digits: 2) }
+    discount { Faker::Number.number(digits: 1) }
+    association :category
   end
 end
