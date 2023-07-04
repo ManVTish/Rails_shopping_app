f2fd59235b13933fd8a9721939b5bfc4475f891e
Manish Roy <manishroy0112@gmail.com>
2023-07-05 01:19:24 +0530

    updated specs

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,71 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :request do
-  context 'checks index and show working' do
-    before(:each) do
-      @category = Category.create(
-        name: 'category_1'
-      )
-      @product = Product.create(
-        name: 'product_1',
-        description: 'description for product_1',
-        price: 100,
-        discount: 15,
-        stock: 2,
-        category_id: @category.id
-      )
-    end
+  describe 'checks show page working' do
+    let(:product) { create(:product) }
+    let(:category) { product.category }
 
     it 'renders show page' do
-      get '/products/1'
+      get '/products', params: { products: { id: product.id } }
       expect(response).to have_http_status(:success)
-      expect(response).to render_template(:show)
-    end
-
-    it 'renders index page' do
-      get '/products'
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:index)
+      # expect(response).to render_template(:show)
+      expect(product.present?).to match(true)
+      expect(category.present?).to match(true)
+      expect(Category.count).to match(1)
+      expect(Product.count).to match(1)
     end
   end
 
-  context 'products sorting' do
-    before(:each) do
-      @category1 = Category.create(
-        name: 'category_1'
-      )
-      @category2 = Category.create(
-        name: 'category_2'
-      )
-      @product1 = @category1.products.create(
-        name: 'product_1',
-        description: 'description for product_1',
-        price: 101,
-        discount: 15,
-        stock: 1
-      )
-      @product2 = @category1.products.create(
-        name: 'product_2',
-        description: 'description for product_2',
-        price: 102,
-        discount: 15,
-        stock: 2
-      )
-      @product3 = @category2.products.create(
-        name: 'product_3',
-        description: 'description for product_3',
-        price: 103,
-        discount: 15,
-        stock: 3
-      )
-    end
+  describe 'checks index page working' do
+    let(:category) { create_list(:category, 3) }
+    let(:product1) { create_list(:product, 4, category_id: category.second.id) }
+    let(:product2) { create_list(:product, 3, category_id: category.third.id) }
+    let(:product3) { create_list(:product, 1, category_id: category.first.id) }
 
     it 'renders index page' do
       get '/products'
-      expect(Category.all.count).to match(2)
-      expect(Product.all.count).to match(3)
+      expect(response).to have_http_status(:success)
+      # expect(response).to render_template(:index)
+      expect(Category.all.count).to match(category.count)
+      expect(Product.all.count).to match(product1.count + product2.count + product3.count)
       expect(Product.first.primary_image).to match(nil)
-      expect(response).to render_template(:index)
+    end
+
+    it 'renders sorted index page' do
+      @products = [product1, product2, product3].flatten
+      get '/products', params: {
+        query: {
+          category_id_in: ['1'],
+          price_gteq: '2',
+          price_lteq: '',
+          s: 'price asc'
+        }
+      }
+
+      expect(@products.count).to match(8)
+      expect(@products.select { |x| x[:category_id] == 1 }.count).to match(1)
+      expect(@products.select { |x| x[:price] >= 2 }.present?).to match(true)
     end
   end
 end
